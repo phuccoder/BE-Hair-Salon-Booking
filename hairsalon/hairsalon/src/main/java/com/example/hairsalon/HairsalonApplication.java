2eba7369bce2eedc940e6750d844823866e81efd
phuccoder <anhdeptraippt@gmail.com>
2024-09-13 10:36:07 +0700

    Setup environment

---
+++ hairsalon/hairsalon/src/main/java/com/example/hairsalon/HairsalonApplication.java
@@ -0,0 +1,13 @@
+package com.example.hairsalon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HairsalonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HairsalonApplication.class, args);
+	}
+
+}
