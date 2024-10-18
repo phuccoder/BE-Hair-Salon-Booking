cdc3d0a273992d8e5c912107f249b5cfc1bccecc
phuccoder <anhdeptraippt@gmail.com>
2024-10-18 08:42:51 +0700

    fix validate service

--- hairsalon/hairsalon/src/main/java/com/example/hairsalon/repositories/ServiceRepository.java
+++ hairsalon/hairsalon/src/main/java/com/example/hairsalon/repositories/ServiceRepository.java
@@ -1,7 +1,10 @@
 package com.example.hairsalon.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.example.hairsalon.models.Services;
 
 public interface ServiceRepository extends JpaRepository<Services, Integer> {
+
+    boolean existsByServiceName(String serviceName);
 }
