0c3cd673c453a31ff837c1aa810a9b611c069c14
phuccoder <anhdeptraippt@gmail.com>
2024-10-28 22:32:42 +0700

    fix api for customer

--- hairsalon/hairsalon/src/main/java/com/example/hairsalon/controllers/services/ServiceControllerByCustomer.java
+++ hairsalon/hairsalon/src/main/java/com/example/hairsalon/controllers/services/ServiceControllerByCustomer.java
@@ -5,17 +5,18 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.hairsalon.models.Services;
-import com.example.hairsalon.requests.ServiceRequest;
 import com.example.hairsalon.services.ServicesService;
 
+@RestController
 @RequestMapping("/api/customer/services")
+@PreAuthorize("hasAnyRole('CUSTOMER')")
 public class ServiceControllerByCustomer {
 
     @Autowired
