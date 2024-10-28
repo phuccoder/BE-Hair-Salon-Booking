e43a8354886d754623ada199df9e90fd6337c642
phuccoder <anhdeptraippt@gmail.com>
2024-10-28 22:28:20 +0700

    add combo for customer

--- hairsalon/hairsalon/src/main/java/com/example/hairsalon/controllers/combos/ComboControllerByCustomer.java
+++ hairsalon/hairsalon/src/main/java/com/example/hairsalon/controllers/combos/ComboControllerByCustomer.java
@@ -7,13 +7,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.example.hairsalon.requests.ComboRequest;
 import com.example.hairsalon.services.ComboService;
 
+@RestController
 @RequestMapping("/api/customer/combos")
 @PreAuthorize("hasAnyRole('CUSTOMER')")
 public class ComboControllerByCustomer {
